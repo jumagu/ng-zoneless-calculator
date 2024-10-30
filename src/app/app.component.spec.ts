7148fb8db2a4c7c02e270eaa5029350a45d9abbc
jumagu <jmgg.gutierrez@gmail.com>
2024-10-29 22:05:05 -0500

    test: add unit tests for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'zoneless-calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('zoneless-calculator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  it('should render router-outlet', () => {
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
+  });
+
+  it('should render the router-outlet inside a div with specific tailwind classes', () => {
+    const div = compiled.querySelector('div');
+    const tailwindClasses =
+      'min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5'.split(
+        ' '
+      );
+
+    expect(div).toBeTruthy();
+    // div?.classList.forEach((cls) => {
+    //   expect(tailwindClasses).toContain(cls);
+    // });
+    tailwindClasses.forEach((cls) => {
+      expect(div?.classList).toContain(cls);
+    });
   });
 });
